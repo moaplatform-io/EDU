e25f2adae01ce644b0e84de187eff9533d19186c
hyeindev <whgpdls1996@gmail.com>
2020-09-22 14:51:01 +0900

    xv

--- src/he/HE.java
+++ src/he/HE.java
@@ -1,10 +1,27 @@
 package he;
 
+import java.util.ArrayList;
+
 public class HE {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("eh");
+		ArrayList al = new ArrayList();
+
+		int n = 0;
+
+		for (int i = 1; i <= 7; i++) {
+
+			n = (int) (Math.random() * 46 + 1);
+
+			for (int j = 0; j < al.size(); j++) {
+				int n2 = (int) al.get(j);
+				if (n == n2) {
+					break;
+				}
+				al.add(n);
+			}
+			System.out.print(n + ", ");
+		}
 	}
 
 }
