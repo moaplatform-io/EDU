c617ea4bcfe10ddeaee828b4855daa8ca327a261
tim <smkim@moaplatform.io>
2020-09-22 16:11:56 +0900

    ddd

---
+++ src/sm/Test.java
@@ -0,0 +1,19 @@
+package sm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		List a=new ArrayList();
+		a=new Vector();
+		
+		a.add(null);
+		
+		
+		System.out.println(a.size());
+		
+	}
+}
