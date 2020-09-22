7ea2dce653fca6d183492cf8fbb81d19aa74353c
모아 혁신개발실 < @DESKTOP-UQDH4MC>
2020-09-22 15:06:54 +0900

    dddd

--- src/he/HE.java
+++ src/he/HE.java
@@ -1,27 +1,59 @@
 package he;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class HE {
 
-	public static void main(String[] args) {
+	static public List<Integer>  makeLOto(String[] args) {
 		ArrayList al = new ArrayList();
 
 		int n = 0;
 
-		for (int i = 1; i <= 7; i++) {
-
-			n = (int) (Math.random() * 46 + 1);
-
-			for (int j = 0; j < al.size(); j++) {
-				int n2 = (int) al.get(j);
-				if (n == n2) {
-					break;
-				}
+		while(al.size()==7) {
+			
+			n=makeLOto();
+			
+			if(isDuplipcat(al, n)) {
+				
+			}else {
 				al.add(n);
 			}
-			System.out.print(n + ", ");
 		}
+		return al;
+	}
+	
+	public static int makeLOto() {
+		int n=0;
+		n = (int) (Math.random() * 46 + 1);
+		return n;
+	}
+		
+	public static boolean isDuplipcat(List<Integer> list,int n) {
+		boolean isDup=false;
+		
+		return isDup;
+	}
+	
+	public static void main(String [] args) {
+		System.out.println("go check");
+		boolean isRoop=true;
+		
+		int cnt=0;
+		do {
+			List<Integer> lotos=makeLOto(args);
+			
+			if(lotos.size()<7) {
+				System.out.println(cnt);
+				System.out.println(lotos);
+				break;
+			}
+			if((cnt++)%1000==0) {
+				System.out.println("ok :"+cnt);
+			}
+		}while(isRoop);
+		
+		
 	}
 
 }
