0fbae4f467bf537da43a7475a3e6b7d91750e8f7
모아 혁신개발실 < @DESKTOP-UQDH4MC>
2020-09-22 13:55:59 +0900

    s

---
+++ src/he/HE.java
@@ -0,0 +1,10 @@
+package he;
+
+public class HE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("eh");
+	}
+
+}
---
+++ src/sg/SG.java
@@ -0,0 +1,10 @@
+package sg;
+
+public class SG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ehllo");
+	}
+
+}
